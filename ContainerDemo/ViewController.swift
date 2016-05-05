fa40e5229f7302d6892b92d43df49deab177c61b
三斗 <sandou@sandou.local>
2016-05-05 10:27:33 +0800

    start
    start

---
+++ ContainerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerDemo
+//
+//  Created by 三斗 on 5/5/16.
+//  Copyright © 2016 com.streamind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
